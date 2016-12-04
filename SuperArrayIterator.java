6e8dbac4fe6f9146333c6c96cdfd9f519494ae54
tmkrocks138 <tmkrocks138@users.noreply.github.com>
2016-12-04 15:03:09 -0500

    Create SuperArrayIterator.java

---
+++ SuperArrayIterator.java
@@ -0,0 +1,11 @@
+public class SuperArrayIterator{
+    private SuperArray sa;
+    private int count;
+
+    public SuperArrayIterator(SuperArray soup, int whichEle){
+	     sa = soup;
+	     count= whichEle;
+    }
+
+
+}
